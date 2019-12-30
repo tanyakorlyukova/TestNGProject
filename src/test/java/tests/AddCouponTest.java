25338f84672749ed2a8fa8244de5fd2a4edd5d85
tatiana.korliukova <tatiana.korliukova@intexsoft.by>
2019-12-30 18:03:53 +0300

    add tests for coupon

--- src/test/java/tests/AddCouponTest.java
+++ src/test/java/tests/AddCouponTest.java
@@ -1,4 +1,24 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.CouponPage;
+import pages.LoginPage;
+import pages.MainPage;
+
 public class AddCouponTest extends BaseTest {
+
+    @BeforeMethod
+    public void login() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.logIn("demo@open-eshop.com", "demo");
+    }
+
+    @Test
+    public void addCoupon() {
+        MainPage mainPage = new MainPage(driver);
+        CouponPage couponPage = mainPage.openCouponsPage();
+
+
+    }
 }
