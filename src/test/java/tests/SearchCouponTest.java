5e8bb3af791137e933fc729fc9e7201980b1e94b
Tanya Korlyukova <36532585+tanyakorlyukova@users.noreply.github.com>
2020-01-13 10:07:02 +0300

    update search coupon test

--- src/test/java/tests/SearchCouponTest.java
+++ src/test/java/tests/SearchCouponTest.java
@@ -9,21 +9,20 @@ import pages.MainPage;
 
 public class SearchCouponTest extends BaseTest {
 
-    String couponName = "Coupon876876786587768876";// + Math.random()*1000;
+    //change coupon name before test
+    String couponName = "Coupon0909090909";
 
     @BeforeMethod
-    public void addAndSearchCoupon() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.logIn("demo@open-eshop.com", "demo");
+    public void addCoupon() {
+        new LoginPage(driver).logIn("demo@open-eshop.com", "demo");
         MainPage mainPage = new MainPage(driver);
         CouponPage couponPage = mainPage.openCouponsPage();
+        couponPage.openNewCouponPage();
         couponPage.addCoupon(couponName, "5", "2020-01-23", "6");
     }
 
     @Test void searchCoupon() {
-        MainPage mainPage = new MainPage(driver);
-        CouponPage couponPage = mainPage.openCouponsPage();
-        couponPage.searchCoupon(couponName);
+        CouponPage couponPage = new CouponPage(driver).searchCoupon(couponName);
         Assert.assertTrue(couponPage.isCouponFound());
     }
 }
